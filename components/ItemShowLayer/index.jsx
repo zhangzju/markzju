ae7929477285897b56763ce1da1a5a49b0339ef4
zhangzju <zhangway19921221@163.com>
2017-02-13 18:05:06 +0800

    add marked

---
+++ components/ItemShowLayer/index.jsx
@@ -0,0 +1,29 @@
+import marked from 'marked';
+import React, {PropTypes} from 'react';
+
+
+function ItemShowLayer({item}) {
+    //if no item nor item.id passed in, return some tips
+    if(!item || !item.id) {
+        return (
+            <div className="col-md-8 item-show-layer-component">
+                <div className="no-select">Please choose an article!</div>
+            </div>
+        );
+    }
+
+    //transfer Markdown to html
+    let content = marked(item.content);
+    return (
+        <div className="col-md-8 item-show-layer-component">
+            <div className="control-area">
+                <button className="btn btn-primary">Edit</button>
+                <button className="btn btn-danger">Delete</button>
+            </div>
+            <h2>{item.title}</h2>
+            <div className="item-text">
+                <div dangerouslySetInnerHTML={{__html: content}} />
+            </div>
+        </div>
+    )
+}
